6625b39b3bb051b046de1b3acfd013bf3f6f9e6f
nicholaskamwaki <122615230+nicholaskamwaki@users.noreply.github.com>
2023-01-21 06:19:41 -0800

    Add files via upload

---
+++ Product of XandY.C
@@ -0,0 +1,13 @@
+//arithmetic operators producs
+#include<stdio.h>
+int main()
+{
+int x,y,prod;
+printf("\n enter value of x:");
+scanf("%d",&x);
+printf("\n enter value of y:");
+scanf("%d",&y);
+prod=x*y;
+printf("the product=%d",prod);
+return 0;
+}
