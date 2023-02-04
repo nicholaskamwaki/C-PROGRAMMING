69f01dcaeddbe372c5e5bc1541095ddd5681563f
nicholaskamwaki <122615230+nicholaskamwaki@users.noreply.github.com>
2023-02-04 13:51:28 +0300

    Add files via upload

---
+++ divisibility_of_five.c
@@ -0,0 +1,13 @@
+// divisibility test of five
+#include<stdio.h>
+int main () 
+{
+    int a;
+    printf(" Enter a value: ");
+    scanf("%d",&a);
+    if (a%5==0) {
+        printf("a value is divisible by 5"); }
+        else {printf ("a is not divisible by 5");
+        }
+        return 0;
+    }
