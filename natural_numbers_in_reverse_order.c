1aa28ffdd01e991637f90e368681daac5d69aa08
nicholaskamwaki <122615230+nicholaskamwaki@users.noreply.github.com>
2023-02-21 15:59:34 +0300

    Add files via upload

---
+++ natural_numbers_in_reverse_order.c
@@ -0,0 +1,19 @@
+//natural numbers in reverse order
+#include<stdio.h>
+int main ()
+{
+    int num;
+    
+    printf("Enter a positive number\n");
+    scanf("%d", &num);
+    
+    printf("\nNatural numbers from 1 to %d are:\n", num);
+    
+    while(num)
+    {
+        printf("%d ",num);
+        num--;
+    }
+    printf("\n");
+    return 0;
+    }
