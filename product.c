cc96b8d000cd33a74ed730a0ee60cadece188255
nicholaskamwaki <122615230+nicholaskamwaki@users.noreply.github.com>
2023-03-01 16:44:03 +0300

    Add files via upload

---
+++ product.c
@@ -0,0 +1,13 @@
+//arithmetic operators products
+#include<stdio.h>
+int main()
+{
+int num1,num2,prod;
+printf("\n enter value of num1:");
+scanf("%d",&num1);
+printf("\n enter value of num2:");
+scanf("%d",&num2);
+prod=num1*num2;
+printf("the product=%d",prod);
+return 0;
+}
